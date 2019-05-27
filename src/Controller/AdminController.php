2b18def6dd81813dc647b92bef9118596e90ec82
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2019-05-27 13:09:38 +0100

    Admin CRUD complete

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,9 +6,8 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -49,6 +48,40 @@ class AdminController extends AbstractController
         return $this->render('forms/user.html.twig', [
             'user_form' => $userForm->createView(),
             'form_title'  => 'Add new user',
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}/remove", name="user_delete")
+     */
+    public function deleteUser(User $user, EntityManagerInterface  $entityManager) {
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin');
+    }
+
+    /**
+     * @Route("/user/{id}/edit", name="user_edit")
+     */
+    public function editUser(User $user, Request $request, EntityManagerInterface  $entityManager) {
+
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->remove('password');
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('forms/user.html.twig', [
+            'user_form' => $userForm->createView(),
+            'form_title'  => "Edit user: {$user->getName()}",
+            'button_label'  => "Update details",
         ]);
     }
 }
